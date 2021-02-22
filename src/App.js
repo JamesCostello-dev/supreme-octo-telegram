1c741c747fabf0879651f9a89725aa8fdd397336
JamesCostello-dev <jcotab@gmail.com>
2021-02-21 19:12:28 -0700

    contact complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import About from "./components/About";
 import Nav from "./components/Nav";
 import Gallery from "./components/Gallery";
-import "./App.css";
 import ContactForm from "./components/Contact";
 
 function App() {
@@ -20,6 +19,7 @@ function App() {
     },
   ]);
 
+  const [contactSelected, setContactSelected] = useState(false);
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
@@ -28,11 +28,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
